c70842fa5d822f3e305643033ed2741816c109b7
Drew Pilcher <drewpilcher@Drews-MacBook-Pro.local>
2016-10-16 12:34:39 +0800

    changed stout

--- main.py
+++ main.py
@@ -16,25 +16,31 @@ def main(first_database_page, number_of_pages):
 	audio_files = pool.map(get_thing_information, database_urls)
 
 def get_thing_information(database_url):
-	print('processing ' + database_url)
 	response = requests.get(database_url, cookies = cookies)
 	tree = html.fromstring(response.text)
 	things = tree.xpath("//tr[contains(@class, 'thing')]")
 	for thing in things:
 		thing_attributes = thing.xpath("td[contains(@class, 'text')]")
+		thing_id = thing.attrib['data-thing-id']
+		successful_attributes = [thing_id]
 		for thing_attribute in thing_attributes:
-			thing_id = thing.attrib['data-thing-id']
 			cell_id = thing_attribute.attrib["data-key"]
 			cell_type = thing_attribute.attrib["data-cell-type"]
 			try:
 				inner_content = thing_attribute.xpath("div/div[contains(@class, 'text')]/text()")[0]
+				successful_attributes.append(inner_content)
+				# print('Processing ID' + thing_id + ': ' + inner_content)
 				post = requests.post('http://www.memrise.com/ajax/thing/cell/update/', cookies=cookies, data={'thing_id':thing_id, 'cell_id':cell_id, 'cell_type':cell_type, 'new_val':inner_content}, headers=headers)
 			except IndexError:
+				successful_attributes.append('N/A')
 				continue
 			except Exception as e:
-				print ('OOPS!! Something strange happened for when processing ' + inner_content + '. Please check the course database for this word to make sure that everything looks okay. Here is the error that caused a problem:')
+				successful_attributes.append('FAILED')
+				print ('OOPS!! Something strange happened when processing ' + inner_content + '. Please check the course database for this word to make sure that everything looks okay. Here is the error that caused a problem:')
 				print (e)
 				continue
+		print(successful_attributes)
+
 
 if __name__ == "__main__":
 	if 'Login' in str(requests.get(course_database_url, cookies=cookies).content):
